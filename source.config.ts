a762691fb53cd4ff0f21dd7bef215542263e4399
wangenius <wangenius@qq.com>
2025-11-17 23:23:56 +0800

    update

--- source.config.ts
+++ source.config.ts
@@ -11,7 +11,7 @@ import {
   remarkHeading,
   remarkDirectiveAdmonition,
 } from "fumadocs-core/mdx-plugins";
-
+import { remarkMdxMermaid } from "fumadocs-core/mdx-plugins";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import remarkDirective from "remark-directive";
@@ -42,6 +42,7 @@ export default defineConfig({
       remarkMath,
       remarkImage,
       remarkHeading,
+      remarkMdxMermaid,
     ],
     rehypePlugins: (v) => [rehypeKatex, ...v],
     remarkImageOptions: {
