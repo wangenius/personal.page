797c378abe1d87e11213313fa68d6d6252897f5f
wangenius <wangenius@qq.com>
2025-11-17 21:02:48 +0800

    update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,15 @@
-import { PersonalProfile } from "@/components/personal-profile";
+import { Contact } from "@/components/Contact";
+import { Hero } from "@/components/Hero";
+import { Timeline } from "@/components/Timeline";
 
 export default function HomePage() {
-  return <PersonalProfile />;
+  return (
+    <div className="flex-1 overflow-auto">
+      <div className="max-w-6xl px-4 mx-auto py-12 md:py-16">
+        <Hero />
+        <Timeline />
+        <Contact />
+      </div>
+    </div>
+  );
 }
