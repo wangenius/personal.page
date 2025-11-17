b263e9cd9f0825de105908087e61841ee04c9fc5
wangenius <wangenius@qq.com>
2025-11-17 21:30:43 +0800

    udpate

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,8 @@
+"use client";
 import { Contact } from "@/components/Contact";
+import {
+  SelectField
+} from "@/components/docs/selection-quote";
 import { Hero } from "@/components/Hero";
 import { Timeline } from "@/components/Timeline";
 
@@ -6,9 +10,15 @@ export default function HomePage() {
   return (
     <div className="flex-1 overflow-auto">
       <div className="max-w-6xl px-4 mx-auto py-12 md:py-16">
-        <Hero />
-        <Timeline />
-        <Contact />
+        <SelectField
+          onSelect={(text) => ({
+            text,
+          })}
+        >
+          <Hero />
+          <Timeline />
+          <Contact />
+        </SelectField>
       </div>
     </div>
   );
